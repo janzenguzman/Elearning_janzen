7563d6d1201fbad7f72776d3e260393e397083e0
janzenguzman <janzenguzman@gmail.com>
2020-10-18 21:15:37 +0800

    Create Login module, Sessions controller and static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  #Static Pages
+  get '/about-us', to:'pages#about'
+  get '/contact-us', to:'pages#contact'
+
+  #Users
+  #root 'users#index'
   get '/signup', to: 'users#new'
 
+  #Sessions
+  root 'sessions#new'
+  delete 'logout', to: 'sessions#destory'
+  get 'sessions/create'
+
   resources :users
+  resources :sessions, only: :create
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
