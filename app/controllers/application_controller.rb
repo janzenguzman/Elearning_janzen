a1f61b1563efaba722cbb5ce5fae217bf40dcaee
janzenguzman <janzenguzman@gmail.com>
2020-10-19 17:49:46 +0800

    User index and show pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+  #Authenticate User login
+  def logged_in_user
+    unless logged_in?
+      redirect_to root_url
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   
-  before_action :user_id, only: [:edit, :update]
+  before_action :logged_in_user, except: [:new, :create]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :user_id, only: [:edit, :update, :show]
+
+  def index
+    @users = User.all
+  end
 
   def new
     @user = User.new
@@ -13,6 +19,9 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def show
   end
 
   def edit
@@ -33,5 +42,12 @@ class UsersController < ApplicationController
 
     def user_id
       @user = User.find(params[:id])
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      if @user != current_user
+        redirect_to root_url
+      end
     end
 end
