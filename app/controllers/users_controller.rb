1cfcac8fb38487ea6285f1c93292809f728f8f7f
janzenguzman <janzenguzman@gmail.com>
2020-10-19 16:40:20 +0800

    User Edit and Update (#3)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,37 @@
 class UsersController < ApplicationController
+  
+  before_action :user_id, only: [:edit, :update]
+
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new(params.require(:user).permit(:name, :email, :password, :password_confirmation))
+    @user = User.new(user_params)
     if @user.save
       redirect_back(fallback_location: request.referer)
     else
       render 'new'
     end
   end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_back(fallback_location: request.referer)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def user_id
+      @user = User.find(params[:id])
+    end
 end
