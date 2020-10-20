89578c81ed611c6040f5c05f6885385a93656653
janzenguzman <janzenguzman@gmail.com>
2020-10-20 17:54:44 +0800

    Create following and follower Relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,31 @@ class User < ApplicationRecord
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
                                     uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, length: { minimum: 6 }, allow_nil: true 
+  validates :password, length: { minimum: 6 }, allow_nil: true
+
+  #Relationship
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                  foreign_key: "followed_id",
+                                  dependent: :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
+  #Follow a user
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  #Unfollow a user
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  #Return true if the current user is following the other user
+  def following?(other_user)
+    active_relationships.find_by(followed_id: other_user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :sessions, only: :create
+  #Relationship
+  resources :relationships, only: [:create, :destory]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
