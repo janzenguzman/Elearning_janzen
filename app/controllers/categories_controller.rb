48cc69649f328ce09c266f1335b10489016dcc43
janzenguzman <janzenguzman@gmail.com>
2020-10-23 08:26:16 +0800

    Category: show page, edit and update, and delete (#7)

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController < ApplicationController
 
   before_action :admin_log_in
+  before_action :cat_id, only: [:edit, :update, :show]
 
   def index
     @category = Category.all
@@ -20,8 +21,34 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(cat_params)
+      flash[:success] = "Category updated."
+      redirect_to categories_url
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id]).destroy
+    @category.destroy
+    flash[:danger] = "Category deleted."
+    redirect_back(fallback_location: request.referer)
+  end
+
   private
     def cat_params
       params.require(:category).permit(:title, :description)
+    end
+
+    def cat_id
+      @category = Category.find(params[:id])
     end
 end
